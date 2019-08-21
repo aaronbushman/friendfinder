282bb186e3e2ce295b29a4b74fabd7c557ed2dfc
aaron <aaronbushman@gmail.com>
2019-08-21 17:56:08 -0400

    functional app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,12 +5,36 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    app.post("/api/friends", function(req, res) {
-        
-        var newFriend = req.body;
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-        console.log(newFriend);
-        friendsArray.push(newFriend)
-        res.json(newcharacter)
+    app.post("/api/friends", function (req, res) {
+
+        var bestDifferent = 50;
+        var matchScore = 0;
+
+        for (var i = friendsData.length - 1; i >= 0; i--) {
+            console.log("Comparing with " + friendsData[i].name);
+
+            var totalDifference = 0;
+
+            for (var j = 0; j < 10; j++) {
+                totalDifference = totalDifference + Math.abs(friendsData[i].scores[j] - req.body.scores[j]);
+            }
+            if (totalDifference < bestDifferent) {
+                bestDifferent = totalDifference
+                matchScore = i;
+            }
+            console.log("Total difference: " + friendsData[i].name + " is " + totalDifference);
+        }
+
+        console.log("-----------------------------");
+        console.log("best person is " + friendsData[matchScore].name + " and best score is " + bestDifferent);
+        console.log("-----------------------------");
+
+        // push in the user input into the friendArray
+        friendsData.push(req.body);
+
+        // respond back with the best match
+        res.json({ name: friendsData[matchScore].name, photo: friendsData[matchScore].photo });
+        // res.json(true);
     });
-};
+}
+
